f966f339a77532f495b4ce2cb0c26eabe7ae860a
popaab <kkl34@sfu.ca>
2015-02-28 23:19:58 -0800

    initial commit

---
+++ scripts/controllers/contact.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clockApp.controller:ContactCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the clockApp
+ */
+angular.module('clockApp')
+  .controller('ContactCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
