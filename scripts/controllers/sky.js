c0f757582a06e2c61470e70163829ef481d2e54f
Mio Hayashi <miohayashi@Mio-Hayashiteki-iMac.local>
2015-03-03 00:24:28 -0800

    select

--- scripts/controllers/sky.js
+++ scripts/controllers/sky.js
@@ -256,7 +256,7 @@ var mc = new Hammer.Manager(document.body);
         if ( intersects.length > 0 ) {
 
           intersects[ 0 ].object.material.color.setHex( Math.random() * 0xffffff );
-          console.log(intersects[0].object.postion.x);
+          console.log(intersects[0].object.getPostion().x);
 
           // document.getElementById("resultDIV").innerHTML = "tap: "+ x1 + " " + y1;
 
