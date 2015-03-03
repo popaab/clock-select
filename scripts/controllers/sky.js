3acf0cf539d09cfa7bdf8087546b649416b57a83
popaab <kkl34@sfu.ca>
2015-03-03 15:18:31 -0800

    initial

--- scripts/controllers/sky.js
+++ scripts/controllers/sky.js
@@ -285,23 +285,23 @@ var mc = new Hammer.Manager(document.body);
                 if( event.pointerType === "touch"){
 
                       editMode = true;
-                      // touchPos.x = ( event.pointers[0].clientX/ renderer.domElement.width ) * 2 - 1;
-                      // touchPos.y = - ( event.pointers[0].clientY / renderer.domElement.height ) * 2 + 1;
+                      touchPos.x = ( event.pointers[0].clientX/ renderer.domElement.width ) * 2 - 1;
+                      touchPos.y = - ( event.pointers[0].clientY / renderer.domElement.height ) * 2 + 1;
 
-                      // raycaster.setFromCamera( touchPos, camera );
+                      raycaster.setFromCamera( touchPos, camera );
 
-                      // var intersects = raycaster.intersectObjects( targetList );
+                      var intersects = raycaster.intersectObjects( targetList );
                     
-                      // if ( intersects.length > 0 ) {
+                      if ( intersects.length > 0 ) {
 
-                      //   intersects[ 0 ].object.material.color.setHex( Math.random() * 0xffffff );
-                      //   console.log(intersects[0].object.id);
-                      //   var k = targetList.indexOf(intersects[0].object.id);
-                      //   selected = intersects[0].object;
-                      //   var cameraX = selected.position.x;
-                      //   var cameraY = selected.position.y;
+                        intersects[ 0 ].object.material.color.setHex( Math.random() * 0xffffff );
+                        console.log(intersects[0].object.id);
+                        var k = targetList.indexOf(intersects[0].object.id);
+                        selected = intersects[0].object;
+                        var cameraX = selected.position.x;
+                        var cameraY = selected.position.y;
 
-                      //   var cameraZ = -selected.position.z + 200;
+                        var cameraZ = -selected.position.z + 200;
                         removeEntity(selected);
 
                         console.log(cameraX,cameraY,cameraZ);
