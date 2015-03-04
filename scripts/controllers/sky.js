fbed88ee20bd59a8ad65b515ecb3657b11e24352
popaab <kkl34@sfu.ca>
2015-03-04 02:59:25 -0800

    last changes

--- scripts/controllers/sky.js
+++ scripts/controllers/sky.js
@@ -534,7 +534,7 @@ function makeTextSprite( message, parameters )
                   + borderColor.b + "," + borderColor.a + ")";
 
   context.lineWidth = borderThickness;
-  roundRect(context, borderThickness/2, borderThickness/2, textWidth + borderThickness, fontsize * 1.4 + borderThickness, 6);
+  
   // 1.4 is extra height factor for text below baseline: g,j,p,q.
   
   // text color
