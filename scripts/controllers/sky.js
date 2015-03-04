d61f41f321a1474d28cf8fc5e67f4671815ed92e
popaab <kkl34@sfu.ca>
2015-03-04 09:00:15 -0800

    last changes

--- scripts/controllers/sky.js
+++ scripts/controllers/sky.js
@@ -243,7 +243,7 @@ function addOcta(x,y,z) {
 
   var h = getRandom(0, 24);
   var m = getRandom(0, 60);
-    var spritey2 = makeTextSprite(h + " : " + m,    { fontsize: 32, fontface: "Lato"} );
+    var spritey2 = makeTextSprite(Math.floor(h) + " : " + Math.floor(m),    { fontsize: 32, fontface: "Lato"} );
   spritey2.position.set(+200 , +20 , 10 );
   if( amountNow <= maxAlarms){
 
