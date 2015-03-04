22c3af782e2afb532f10e6cf3c789a2e5664d3a3
popaab <kkl34@sfu.ca>
2015-03-04 00:45:44 -0800

    last changes

--- scripts/controllers/sky.js
+++ scripts/controllers/sky.js
@@ -444,7 +444,7 @@ function render()
 {
   renderer.render( scene, camera );
   alarm(selectedObject);
-  scene.getObjectByName('clock').rotation.x += 0.05;
+  // scene.getObjectByName('clock').rotation.x += 0.05;
   deltaTime = clock.getDelta();
   particleSystem.rotation.y += deltaTime/40;
   // mainTime.rotation.y += 0.05;
