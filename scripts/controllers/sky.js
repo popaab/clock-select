5b806621a1b03fc00ed1bdf79dffb858d8b63de3
popaab <kkl34@sfu.ca>
2015-03-04 08:07:48 -0800

    last changes

--- scripts/controllers/sky.js
+++ scripts/controllers/sky.js
@@ -239,7 +239,7 @@ function addOcta(x,y,z) {
   var wireOcta = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ color: 'white', wireframe: true }));
   var spritey = makeTextSprite( "Alarm " + numb, 
     { fontsize: 32, fontface: "Lato"} );
-  spritey.position.set(+50 , -50 , 10 );
+  spritey.position.set(+50 , +30 , 10 );
 
   if( amountNow <= maxAlarms){
 
